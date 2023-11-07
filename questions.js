a7734779297fe77f261dbb4229b206b6a0ae00f0
Kishore <kishore.d@kalvium.community>
2023-11-07 18:11:43 +0530

    commit

---
+++ questions.js
@@ -0,0 +1,173 @@
+const questions = {
+    easy: [
+        {
+            question: "What is the largest mammal on Earth?",
+            options: ["Elephant", "Blue Whale", "Giraffe", "Lion"],
+            answer: "Blue Whale"
+        },
+        {
+            question: "In which country is the Great Wall of China located?",
+            options: ["Japan", "China", "India", "United States"],
+            answer: "China"
+        },
+        {
+            question: "What is the chemical symbol for gold?",
+            options: ["Go", "Ag", "Ge", "Au"],
+            answer: "Au"
+        },
+        {
+            question: "Who painted the Mona Lisa?",
+            options: ["Vincent van Gogh", "Pablo Picasso", "Leonardo da Vinci", "Michelangelo"],
+            answer: "Leonardo da Vinci"
+        },
+        {
+            question: "What is the capital of Japan?",
+            options: ["Beijing", "Tokyo", "Seoul", "Bangkok"],
+            answer: "Tokyo"
+        },
+        {
+            question: "Which planet is known as the Red Planet?",
+            options: ["Mars", "Venus", "Jupiter", "Saturn"],
+            answer: "Mars"
+        },
+        {
+            question: "What is the largest ocean on Earth?",
+            options: ["Atlantic Ocean", "Indian Ocean", "Arctic Ocean", "Pacific Ocean"],
+            answer: "Pacific Ocean"
+        },
+        {
+            question: "How many continents are there on Earth?",
+            options: ["4", "5", "6", "7"],
+            answer: "7"
+        },
+        {
+            question: "What is the primary gas that makes up the Earth's atmosphere?",
+            options: ["Oxygen", "Nitrogen", "Carbon dioxide", "Hydrogen"],
+            answer: "Nitrogen"
+        },
+        {
+            question: "Who is the author of 'To Kill a Mockingbird'?",
+            options: ["Mark Twain", "George Orwell", "Harper Lee", "J.K. Rowling"],
+            answer: "Harper Lee"
+        },
+        {
+            question: "What is the smallest prime number?",
+            options: ["0", "1", "2", "3"],
+            answer: "2"
+        },
+        {
+            question: "What is the national flower of India?",
+            options: ["Rose", "Lotus", "Sunflower", "Tulip"],
+            answer: "Lotus"
+        },
+        {
+            question: "Who is known as the 'Father of the Nation' in India?",
+            options: ["Jawaharlal Nehru", "Sardar Vallabhbhai Patel", "Mahatma Gandhi", "Subhas Chandra Bose"],
+            answer: "Mahatma Gandhi"
+        },
+        {
+            question: "What is the currency of the United Kingdom?",
+            options: ["Dollar", "Euro", "Pound Sterling", "Yen"],
+            answer: "Pound Sterling"
+        },
+        {
+            question: "Which gas do humans breathe out when they exhale?",
+            options: ["Oxygen", "Nitrogen", "Carbon dioxide", "Hydrogen"],
+            answer: "Carbon dioxide"
+        },
+        {
+            question: "What is the largest desert in the world?",
+            options: ["Gobi Desert", "Sahara Desert", "Atacama Desert", "Arabian Desert"],
+            answer: "Sahara Desert"
+        },
+        {
+            question: "Who discovered the theory of relativity?",
+            options: ["Isaac Newton", "Albert Einstein", "Stephen Hawking", "Galileo Galilei"],
+            answer: "Albert Einstein"
+        },
+        {
+            question: "What is the capital of Australia?",
+            options: ["Sydney", "Melbourne", "Brisbane", "Canberra"],
+            answer: "Canberra"
+        },
+        {
+            question: "What is the chemical symbol for water?",
+            options: ["H2O", "CO2", "O2", "NaCl"],
+            answer: "H2O"
+        },
+        {
+            question: "Which planet is known as the 'Evening Star'?",
+            options: ["Mercury", "Venus", "Mars", "Jupiter"],
+            answer: "Venus"
+        },
+        {
+            question: "Who wrote the 'Harry Potter' book series?",
+            options: ["J.R.R. Tolkien", "George R.R. Martin", "J.K. Rowling", "C.S. Lewis"],
+            answer: "J.K. Rowling"
+        },
+        {
+            question: "What is the largest organ in the human body?",
+            options: ["Liver", "Heart", "Brain", "Skin"],
+            answer: "Skin"
+        },
+        {
+            question: "In which country is the city of Rome located?",
+            options: ["Greece", "Spain", "Italy", "France"],
+            answer: "Italy"
+        },
+        {
+            question: "What is the chemical symbol for oxygen?",
+            options: ["O2", "O", "O3", "Ox"],
+            answer: "O2"
+        },
+        {
+            question: "Who was the first person to walk on the Moon?",
+            options: ["Neil Armstrong", "Buzz Aldrin", "Alan Shepard", "John Glenn"],
+            answer: "Neil Armstrong"
+        },
+        {
+            question: "What is the largest species of penguin?",
+            options: ["Emperor Penguin", "Adélie Penguin", "King Penguin", "Gentoo Penguin"],
+            answer: "Emperor Penguin"
+        },
+        {
+            question: "Which gas is used for filling balloons and makes them float?",
+            options: ["Oxygen", "Carbon dioxide", "Helium", "Nitrogen"],
+            answer: "Helium"
+        },
+        {
+            question: "What is the largest planet in our solar system?",
+            options: ["Earth", "Venus", "Mars", "Jupiter"],
+            answer: "Jupiter"
+        },
+        {
+            question: "What is the chemical symbol for gold?",
+            options: ["Go", "Ag", "Ge", "Au"],
+            answer: "Au"
+        }
+    ],
+    medium: [
+        {
+            question: "Which planet is known as the Red Planet?",
+            options: ["Mars", "Venus", "Jupiter"],
+            answer: "Mars"
+        },
+        {
+            question: "What is the largest mammal in the world?",
+            options: ["Elephant", "Blue Whale", "Giraffe"],
+            answer: "Blue Whale"
+        }
+    ],
+    hard: [
+        {
+            question: "In which year did World War II end?",
+            options: ["1939", "1945", "1950"],
+            answer: "1945"
+        },
+        {
+            question: "Who wrote the play 'Romeo and Juliet'?",
+            options: ["William Shakespeare", "Charles Dickens", "Jane Austen"],
+            answer: "William Shakespeare"
+        }
+    ]
+}
